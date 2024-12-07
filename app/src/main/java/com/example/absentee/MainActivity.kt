77888c235cacb75801c02733c76b00d409d47782
BogdanSakovec <sakovets.b0301.121.24@lntu.edu.ua>
2024-12-07 21:56:32 +0200

    Створений перший екран "Today" за допомогою "Scaffold"

--- app/src/main/java/com/example/absentee/MainActivity.kt
+++ app/src/main/java/com/example/absentee/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.absentee
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,29 +22,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AbsenteeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "ela",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TodayScreen()
+
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TodayScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Back")
+                }
+            )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AbsenteeTheme {
-        Greeting("bob")
+
+        }
+    ) {
+
     }
 }
