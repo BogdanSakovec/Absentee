0e63dd30f15f81fb9b5c33555e2558348a8e67a8
BogdanSakovec <sakovets.b0301.121.24@lntu.edu.ua>
2024-12-08 12:08:37 +0200

    До TopAppBar додано кнопку "х" та змінено текст на "Exit"

--- app/src/main/java/com/example/absentee/MainActivity.kt
+++ app/src/main/java/com/example/absentee/MainActivity.kt
@@ -5,14 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.absentee.ui.theme.AbsenteeTheme
 
@@ -37,10 +44,18 @@ fun TodayScreen() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = "Back")
+                    Text(text = "Exit")
+                },
+                Modifier.background(color = Color.Red),
+                navigationIcon = {
+                    IconButton(
+                        onClick = { }
+                    ) {
+                        Icon(imageVector = Icons.Filled.Close,
+                            contentDescription = "Exit")
+                    }
                 }
             )
-
 
         }
     ) {
