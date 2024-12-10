27864da4f83e9c04144fbdff2f0c0022f2b1c2e2
BogdanSakovec <sakovets.b0301.121.24@lntu.edu.ua>
2024-12-10 19:24:45 +0200

    Створений файл Screens.kt. В нього скопійований код екрану "TodayScreen".

--- app/src/main/java/com/example/absentee/Screens.kt
+++ app/src/main/java/com/example/absentee/Screens.kt
@@ -2,55 +2,23 @@ package com.example.absentee
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.absentee.ui.theme.AbsenteeTheme
-import androidx.compose.material3.TopAppBar as TopAppBar1
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AbsenteeTheme {
-                TodayScreen(applicationContext)
-            }
-        }
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun TodayScreen(context: Context) {
+fun TodayScreen(context: Context,onClick: () -> Unit) {
     Scaffold(
         topBar = {
             TopAppBar(
