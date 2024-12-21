be3455bfdb5af1ede9959dd9aa5a4214e95c4702
BogdanSakovec <sakovets.b0301.121.24@lntu.edu.ua>
2024-12-21 19:13:07 +0200

    Для наочності розмістив весь код MainActivity.kt та знайшов недоліки додаток працюе коректно, екрани перемикаються згідно натиснутих кнопок.

--- app/src/main/java/com/example/absentee/MainActivity.kt
+++ app/src/main/java/com/example/absentee/MainActivity.kt
@@ -20,10 +20,15 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldColors
@@ -33,25 +38,26 @@ import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.absentee.BottomNavigaton.BottomNavigationBar
-import com.example.absentee.BottomNavigaton.NavRoutes
 import com.example.absentee.ui.theme.AbsenteeTheme
 import androidx.compose.material3.TopAppBar as TopAppBar1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Main()
         }
@@ -62,13 +68,85 @@ class MainActivity : ComponentActivity() {
 fun Main(){
     val navController = rememberNavController()
     Column(Modifier.padding(8.dp)) {
-        NavHost(navController, startDestination = NavRoutes.TodayScreen.route,
+        NavHost(navController,
+            startDestination = NavRoutes.TodayScreen.route,
             modifier = Modifier.weight(1f) ) {
 
-            composable(NavRoutes.TodayScreen.route) { TodayScreen() }
-            composable(NavRoutes.TodayScreen.route) { WeekScreen() }
-            composable(NavRoutes.TodayScreen.route) { CityScreen() }
+            composable(NavRoutes.TodayScreen.route) { Today() }
+            composable(NavRoutes.WeekScreen.route) { Week() }
+            composable(NavRoutes.CityScreen.route) { City() }
         }
         BottomNavigationBar(navController = navController)
     }
+}
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    NavigationBar {
+        val backStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = backStackEntry?.destination?.route
+
+        NavBarItems.BarItems.forEach { navItem ->
+            NavigationBarItem(selected = currentRoute == navItem.route,
+                onClick = {
+                    navController.navigate(navItem.route) {
+                        popUpTo(navController.graph.findStartDestination().id)
+                        {saveState = true}
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = {
+                    Icon(imageVector = navItem.image,
+                        contentDescription = navItem.title)
+                },
+                label = {
+                    Text(text = navItem.title)
+                }
+            )
+        }
+    }
+}
+
+object NavBarItems {
+    val BarItems = listOf(
+        BarItem(
+            title = "Today",
+            image = Icons.Filled.Warning,
+            route = "today"),
+        BarItem(
+            title = "Week",
+            image = Icons.Filled.Info,
+            route = "week"
+        ),
+        BarItem(
+            title = "City",
+            image = Icons.Filled.LocationOn,
+            route = "city"
+        )
+    )
+}
+
+data class BarItem(
+    val title: String,
+    val image: ImageVector,
+    val route: String)
+
+@Composable
+fun Today() {
+    Text(text = "Today Screen", fontSize = 30.sp,)
+}
+@Composable
+fun Week() {
+    Text(text = "Week Screen", fontSize = 30.sp,)
+}
+@Composable
+fun City() {
+    Text(text = "City Screen", fontSize = 30.sp,)
+}
+
+sealed class NavRoutes(val route: String) {
+    object TodayScreen: NavRoutes("today")
+    object WeekScreen: NavRoutes("week")
+    object CityScreen: NavRoutes("city")
 }
